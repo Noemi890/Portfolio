d819f30eb9b2f12ffb73a153e7969395dd3baee8
Noemi <noemi.caggiano@gmail.com>
2023-02-28 18:39:00 +0000

    feat: first layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import "./css/App.css"
+import React from "react";
+import "./css/App.css";
 
 function App() {
   return (
     <div className="App">
+      <div className="background">
+        <div className="square_1_layout"></div>
+        <div className="square_2_layout"></div>
+      </div>
     </div>
   );
 }
