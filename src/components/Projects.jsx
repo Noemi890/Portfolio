55c7637147463d6aa8468d5d40ad955ea7982a0d
Noemi <noemi.caggiano@gmail.com>
2023-03-06 15:17:46 +0000

    feat: adding projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import { projects } from "../utils/projects";
-import { Grid, Container, Card, CardActionArea, CardMedia, CardContent, Typography, Paper } from "@mui/material";
+import { Container, Card, CardActionArea, Typography, Grid } from "@mui/material";
 
 const Projects = () => {
   return (
-    <Container>
-      {/* <Paper>My Projects</Paper> */}
-      {/* <Grid container id="projects">
-        {projects.map((project, i) => {
+    <div id="projects">
+    <Container maxWidth="md">
+      <Grid container spacing={2}>
+      {
+        projects.map((p, i) => {
           return (
-            <Grid key={i} xs={4}>
-              <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea><CardMedia component="img"/><CardContent><Typography>{project.name}</Typography></CardContent></CardActionArea></Card>
+            <Grid xs={6}>
+            <Card key={i}>
+              <CardActionArea>
+                <Typography>{p.name}</Typography>
+              </CardActionArea>
+            </Card>
             </Grid>
-          );
-        })}
-      </Grid> */}
+          )
+        })
+      }
+      </Grid>
     </Container>
+      </div>
   );
 };
 
